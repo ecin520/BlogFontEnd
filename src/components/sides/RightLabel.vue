34f60b3698fd0b35660d89fc9e2e18f37671fcfe
ecin520 <875631208@qq.com>
2019-09-17 02:36:40 +0800

    fuck

--- src/components/sides/RightLabel.vue
+++ src/components/sides/RightLabel.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="RightLabel">
-      <a :style="FontStyle" v-for="item in labels">{{item.label}} </a>
+<!--      <a :style="FontStyle" v-for="item in labels">{{item.label}} </a>-->
+      <h2>标签云</h2>
+      <a style="font-size: 15px">Java</a>
+      <a style="font-size: 12px">Python</a>
+      <a style="font-size: 19px">Linux</a>
+      <a style="font-size: 12px">MySql</a>
+      <a style="font-size: 22px">Docker</a>
+      <a style="font-size: 12px">Spring</a>
+      <a style="font-size: 12px">Nio</a>
+      <a style="font-size: 18px">Mina</a>
+      <a style="font-size: 16px">Vue</a>
     </div>
 </template>
 
