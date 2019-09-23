ae41e8287e4a5ed0ebe74889f91c44f349576dc9
ecin520 <875631208@qq.com>
2019-09-23 18:01:12 +0800

    添加状态管理

---
+++ src/store.js
@@ -0,0 +1,13 @@
+
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // host: 'http://148.70.50.70:8081/'
+    host: 'api'
+  }
+})
